45afedd118f6e6bced58061d765b2ffe7c366e38
Kevin Cascio <kevincascio@hotmail.com>
2016-02-09 11:27:39 -0500

    Digital Assignment 1
    Modified a Phasor example into a basic prototype and made pitch
    document.

--- js/main.js
+++ js/main.js
@@ -11,42 +11,178 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
+    
     "use strict";
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
+    var game = new Phaser.Game(300, 300, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
+
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+
+        game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tiles-1', 'assets/tiles-1.png');
+        game.load.spritesheet('dude', 'assets/dude.png', 48, 32);
+        game.load.image('door', 'assets/door.png');
+        game.load.image('background', 'assets/black background.png');
+        game.load.audio('steps1', 'assets/steps1.mp3');
+        game.load.audio('steps2', 'assets/steps2.mp3');
+
     }
-    
-    var bouncy;
-    
+
+    var map;
+    var tileset;
+    var layer;
+    var door;
+    var player;
+    var facing = 'right';
+    var jumpTimer = 0;
+    var cursors;
+    var jumpButton;
+    var bg;
+    var won = false;
+    var nextStep = 1;
+    var steps1;
+    var steps2;
+    var playing = false;
+
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        game.stage.backgroundColor = '#000000';
+
+        bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+        bg.fixedToCamera = true;
+
+        map = game.add.tilemap('level1');
+
+        map.addTilesetImage('tiles-1');
+
+        map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+
+        layer = map.createLayer('Tile Layer 1');
+
+        //  Un-comment this on to see the collision tiles
+        // layer.debug = true;
+
+        layer.resizeWorld();
+
+        game.physics.arcade.gravity.y = 250;
         
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        door = game.add.sprite(789, 369, 'door');
+
+        player = game.add.sprite(32, 32, 'dude');
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+
+        //player.body.bounce.y = 0.2;
+        player.body.collideWorldBounds = true;
+        player.body.setSize(46, 32, 1, 0);
+
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        //player.animations.add('turn', [4], 20, true);
+        player.animations.add('right', [4, 5, 6, 7], 10, true);
+
+        game.camera.follow(player);
+
+        cursors = game.input.keyboard.createCursorKeys();
+        jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        steps1 = game.add.audio('steps1');
+        //steps1
+        steps2 = game.add.audio('steps2');
+
+    }
+
+    function update() {
+
+        if(won)
+            game.pause();
+        
+        if(game.physics.arcade.distanceBetween(player, door) <= 20){
+            var winText = game.add.text(0, 0, "You Won!", { font: "64px Comic Sans", fill: "#ffffff", align: "center" });
+            winText.fixedToCamera = true;
+            winText.cameraOffset.setTo(15, 200);
+            won = true;
+        }
+        
+        if(steps1._isPlaying || steps2._isPlaying)
+            playing = true;
+        else
+            playing = false;
+        
+        if(game.physics.arcade.collide(player, layer) && player.body.velocity.x != 0 && !playing)//!steps1._isPlaying && !steps2._isPlaying)
+            playFx();
+        else if(!playing)//!steps1._isPlaying && !steps2._isPlaying)
+            nextStep = 1;
+            
+
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown)
+        {
+            player.body.velocity.x = -150;
+
+            if (facing != 'left')
+            {
+                player.animations.play('left');
+                facing = 'left';
+            }
+        }
+        else if (cursors.right.isDown)
+        {
+            player.body.velocity.x = 150;
+
+            if (facing != 'right')
+            {
+                player.animations.play('right');
+                facing = 'right';
+            }
+        }
+        else
+        {
+            if (facing != 'idle')
+            {
+                player.animations.stop();
+
+                if (facing == 'left')
+                {
+                    player.frame = 0;
+                }
+                else
+                {
+                    player.frame = 4;
+                }
+
+                facing = 'idle';
+            }
+        }
+
+        if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+        {
+            player.body.velocity.y = -250;
+            jumpTimer = game.time.now + 750;
+        }
+
     }
     
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+    function playFx(){
+        if(!steps1._isPlaying && !steps2._isPlaying)
+        switch (nextStep){
+            case 1:
+                steps1.play("", 0, 1, false, false);
+                nextStep = 2;
+                break;
+                
+            case 2:
+                steps2.play("", 0, 1, false, false);
+                nextStep = 1;
+                break;
+        }
+    }
+
+    function render () {
+
+        // game.debug.text(game.time.physicsElapsed, 32, 32);
+        // game.debug.body(player);
+        // game.debug.bodyInfo(player, 16, 24);
+
     }
 };
